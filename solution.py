4bcca1a5730021bb5523bf414b5355b107250572
ReginaMiKi <129621290+ReginaMiKi@users.noreply.github.com>
2023-04-07 18:35:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,12 +6,12 @@ from scipy.stats import chi2
 
 chat_id = 730773348 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array) -> tuple:
+def solution(p: float, x: np.ndarray) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
     loc = x.mean()
     mean = np.mean(x**2)
-    return loc - mean/scipy.stats.chi2.ppf(1-alpha/2, df=1) / 0.39, \
-           loc - mean/scipy.stats.chi2.ppf(alpha/2, df=1) / 0.39
+    return loc - mean / chi2.ppf(1-alpha/2, df=1) / 0.39, \
+           loc - mean / chi2.ppf(alpha/2, df=1) / 0.39
